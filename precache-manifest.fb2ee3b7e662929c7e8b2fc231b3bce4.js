42a674ae16422954dfd88c5f6f5850ea4df354f6
apostolidhs <john.apostolidi@gmail.com>
2020-04-17 21:32:54 +0000

    Deploying to gh-pages from  @ 38cf723ebe9b12efe7128a2e59e15cc6fc207d27 🚀

---
+++ precache-manifest.fb2ee3b7e662929c7e8b2fc231b3bce4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0222f69a73719263d4d417ed5160b937",
+    "url": "/iwantit/index.html"
+  },
+  {
+    "revision": "15d57cbad9643aa36911",
+    "url": "/iwantit/static/js/2.593001b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iwantit/static/js/2.593001b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b72a521981ec5a99813",
+    "url": "/iwantit/static/js/main.cd8a9c5d.chunk.js"
+  },
+  {
+    "revision": "c8500f310b9cd0c86aa6",
+    "url": "/iwantit/static/js/runtime-main.9035b19e.js"
+  }
+]);
