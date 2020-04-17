bdefb6e716eb591ed1d450efb71b6a99370978b6
apostolidhs <john.apostolidh@gmail.com>
2020-04-18 02:26:22 +0300

    Prepared new dependencies.

---
+++ src/providers/notifications/notification.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Box, Text} from 'grommet';
+
+const Container = styled(Box)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+`;
+
+const Notification = ({type, message}) => {
+  return (
+    <Container animation="fadeIn">
+      <Box
+        background={type === 'info' ? 'neutral-3' : 'status-warning'}
+        pad={{left: 'medium', vertical: 'small', right: 'medium'}}>
+        <Text color="white" size="large">
+          {message}
+        </Text>
+      </Box>
+    </Container>
+  );
+};
+
+export default Notification;
