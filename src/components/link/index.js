0fed9c243bfa8a166b39447caf72013258e7d5fa
apostolidhs <john.apostolidh@gmail.com>
2020-04-18 20:58:11 +0300

    Created skeleton loader component.

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Link as RouterLink} from '@reach/router';
 import {useConfiguration} from 'providers/configuration';
 
-const Link = ({to, children, ...props}) => {
+const Link = ({to, children, className}) => {
   const {basepath} = useConfiguration();
   const routerTo = to[0] === '/' ? `${basepath}${to}` : to;
 
   return (
-    <RouterLink {...props} to={routerTo}>
+    <RouterLink className={className} to={routerTo}>
       {children}
     </RouterLink>
   );
