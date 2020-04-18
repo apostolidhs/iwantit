d3379b755b460fe80c4aa5eba42b35bff8f5a8ac
apostolidhs <john.apostolidh@gmail.com>
2020-04-18 17:22:22 +0300

    Added api endpoint.

--- src/providers/api/getDispatch.js
+++ src/providers/api/getDispatch.js
@@ -1,5 +1,12 @@
 import request from 'helpers/request';
 
 export default apiUrl => ({
-  getCategories: props => request.get(`${apiUrl}/categories`, props)
+  getCategories: props => request(`${apiUrl}/categories`, props),
+  getCategory: (id, props) => request(`${apiUrl}/categories/${id}`, props),
+  getProductsByCategory: (id, {page, limit, sort, order, minPrice: min_price, maxPrice: max_price}, props) =>
+    request(`${apiUrl}/categories/${id}/products`, {
+      params: {page, limit, sort, order, min_price, max_price},
+      ...props
+    }),
+  getProduct: (id, props) => request(`${apiUrl}/products/${id}`, props)
 });
--- src/providers/api/index.test.js
+++ src/providers/api/index.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-import ConfigurationProvider from './index';
+import ConfigurationProvider from 'providers/configuration';
+import ApiProvider from './index';
 
 it('renders', () => {
-  const {getByText} = render(<ConfigurationProvider>myContent</ConfigurationProvider>);
+  const {getByText} = render(
+    <ConfigurationProvider>
+      <ApiProvider>myContent</ApiProvider>
+    </ConfigurationProvider>
+  );
   expect(getByText('myContent')).toBeInTheDocument();
 });
